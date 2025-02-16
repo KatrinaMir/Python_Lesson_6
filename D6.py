efcb940680e3f74400ac5b733f32273a8a83f00f
Мирошниченко Екатерина <katuskakrivakina@gmail.com>
2025-02-17 00:55:27 +0300

    Update D6.py

--- D6.py
+++ D6.py
@@ -2,7 +2,7 @@ def has_digit(password):
 	return any(char.isdigit() for char in password)
 	
 def is_very_long(password):
-	return len(password) > 12
+	return len(password) > 12 
 
 def has_upper_letters(password):
 	return any(char.isupper() for char in password)
