182634383fde7e9282139ce6df819305a196544d
Мирошниченко Екатерина <katuskakrivakina@gmail.com>
2025-02-17 14:38:16 +0300

    Update_D6.py

--- D6.py
+++ D6.py
@@ -1,17 +1,22 @@
 def has_digit(password):
 	return any(char.isdigit() for char in password)
-	
+
+
 def is_very_long(password):
 	return len(password) > 12 
+
 
 def has_upper_letters(password):
 	return any(char.isupper() for char in password)
 
+
 def has_lower_letters(password):
 	return any(char.islower() for char in password)
 
+
 def has_symbols(password):
 	return any(not char.isdigit() and not char.isalpha() for char in password)
+
 
 def main():
 	password = list(input("Введите пароль: "))
